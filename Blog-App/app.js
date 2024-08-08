abc905013e0250a2dd0018a733f619f450b24922
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-08-08 20:55:40 +0530

    routes setup

--- Blog-App/app.js
+++ Blog-App/app.js
@@ -5,10 +5,16 @@ const expressLayout = require('express-ejs-layouts');
 
 const PORT = 5000 || process.env.PORT;
 
+app.use(express.static('public'));
+
 //Templating Engine
 app.use(expressLayout);
 app.set('layout', './layouts/main.ejs');
 app.set('view engine','ejs');
+
+
+app.use('/', require('./server/routes/main'));
+
 
 app.get('/', (req,res) =>{
     res.send("hello, Pavan kalyan. This is a blog website")
