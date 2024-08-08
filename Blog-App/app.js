c4570da929f7f95d756d591a5c8b931545a758f5
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-08-08 20:34:35 +0530

    template engine setup

--- Blog-App/app.js
+++ Blog-App/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const expressLayout = require('express-ejs-layouts');
 
 const PORT = 5000 || process.env.PORT;
+
+//Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main.ejs');
+app.set('view engine','ejs');
 
 app.get('/', (req,res) =>{
     res.send("hello, Pavan kalyan. This is a blog website")
